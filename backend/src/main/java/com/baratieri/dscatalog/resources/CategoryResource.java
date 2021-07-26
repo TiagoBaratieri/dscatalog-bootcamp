b6456ce17ff32b84484a93b19f83f71457de3394
Tiago Baratieri <tiagobaratieri97@gmail.com>
2021-07-26 14:33:57 -0300

    H2 data base

--- backend/src/main/java/com/baratieri/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/baratieri/dscatalog/resources/CategoryResource.java
@@ -1,23 +1,25 @@
 package com.baratieri.dscatalog.resources;
 
 import com.baratieri.dscatalog.entities.Category;
+import com.baratieri.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
+    @Autowired
+    private CategoryService service;
+
     @GetMapping
     public ResponseEntity<List<Category>> findAll(){
-        List<Category> list = new ArrayList<>();
-        list.add(new Category(1l,"Books"));
-        list.add(new Category(2l,"Electronics"));
+     List<Category> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
