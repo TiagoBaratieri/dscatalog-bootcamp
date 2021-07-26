32a01b60c292d1e0e6774f45e3eb0c5d652b8ae4
Tiago Baratieri <tiagobaratieri97@gmail.com>
2021-07-26 16:29:25 -0300

    insert Category

--- backend/src/main/java/com/baratieri/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/baratieri/dscatalog/resources/CategoryResource.java
@@ -4,11 +4,10 @@ import com.baratieri.dscatalog.dto.CategoryDTO;
 import com.baratieri.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,5 +27,13 @@ public class CategoryResource {
     public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
         CategoryDTO catDto = service.findById(id);
         return ResponseEntity.ok().body(catDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{Id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
