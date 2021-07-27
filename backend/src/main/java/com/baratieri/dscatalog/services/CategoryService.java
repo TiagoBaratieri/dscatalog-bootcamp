d21144158bcd63f1716ae398514689cb994ade6d
Tiago Baratieri <tiagobaratieri97@gmail.com>
2021-07-27 14:26:35 -0300

    update category

--- backend/src/main/java/com/baratieri/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/baratieri/dscatalog/services/CategoryService.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
 
 @Service
@@ -37,5 +39,17 @@ public class CategoryService {
        entity.setName(dto.getName());
        entity = repository.save(entity);
        return new CategoryDTO(entity);
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+        try {
+            Category entity = repository.getOne(id);
+            entity.setName(dto.getName());
+            entity = repository.save(entity);
+            return new CategoryDTO(entity);
+        }catch (EntityNotFoundException e){
+           throw new ResourceNotFoundException("Id not found " +id);
+        }
     }
 }
