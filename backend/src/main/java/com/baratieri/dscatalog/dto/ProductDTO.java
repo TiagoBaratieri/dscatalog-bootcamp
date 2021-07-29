3fbbd4b67a5c863ef1cf5d1029d29c4132643336
Tiago Baratieri <tiagobaratieri97@gmail.com>
2021-07-29 14:16:01 -0300

    custom constraintValidator

--- backend/src/main/java/com/baratieri/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/baratieri/dscatalog/dto/ProductDTO.java
@@ -24,6 +24,7 @@ public class ProductDTO  implements Serializable {
 
     @NotBlank(message = "Campo requerido")
     private String description;
+
     @Positive(message = "Preço deve ser um valor positivo")
     private Double price;
     private String imgUrl;
--- backend/src/main/java/com/baratieri/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/baratieri/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.baratieri.dscatalog.dto;
 
+import com.baratieri.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
     private static final long serialVersionUID = 1L;
 
