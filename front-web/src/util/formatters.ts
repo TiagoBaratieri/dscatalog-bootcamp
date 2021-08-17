9b8712ef066c6f3c8c03c60a858735f77076ab00
Tiago Baratieri <tiagobaratieri97@gmail.com>
2021-08-17 15:58:57 -0300

    formatPrice

---
+++ front-web/src/util/formatters.ts
@@ -0,0 +1,5 @@
+export const formatPrice = (price: number) => {
+
+    const params = {maximumFractionDigits : 2, minimunFractionDigits: 2}
+    return new Intl.NumberFormat('pt-BR', params).format(price);
+}
