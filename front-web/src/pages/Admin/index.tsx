ac97c978f3b154f883177c07e8aeace19f13ad5c
Tiago Baratieri <tiagobaratieri97@gmail.com>
2021-08-20 12:01:09 -0300

    Admin navbar layout mobile

--- front-web/src/pages/Admin/index.tsx
+++ front-web/src/pages/Admin/index.tsx
@@ -1,7 +1,14 @@
-const Admin = () => (
-  <div>
-    <h1>Admin</h1>
-  </div>
-);
+import Navbar from './Navbar';
+import './styles.scss';
+const Admin = () => {
+  return (
+    <div className="navbar-container">
+      <Navbar />
+      <div className="admin-content">
+        <h1>Contéudo</h1>
+      </div>
+    </div>
+  );
+};
 
 export default Admin;
