4354f366ac13428d4d82ad2c3ed248d3639a652f
Tiago Baratieri <tiagobaratieri97@gmail.com>
2021-09-16 16:49:17 -0300

    Product crud form HTML

--- front-web/src/pages/Admin/Products/Form/index.tsx
+++ front-web/src/pages/Admin/Products/Form/index.tsx
@@ -1,6 +1,27 @@
-const Form = () => {
-    return <h1> Tela Form</h1>;
-  };
-  
-  export default Form;
-  
+import './styles.scss';
+
+const Form = () => (
+  <div className="product-crud-container">
+    <div className="base-card product-crud-form-card">
+      <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
+      <form action="">
+        <div className="row">
+          <div className="col-lg-6">
+            <input type="text" className="form-control base-input"/>
+            <input type="text" className="form-control base-input"/>
+            <input type="text" className="form-control base-input"/>
+          </div>
+          <div className="col-lg-6">
+            <textarea name="" rows={10} className="form-control base-input"></textarea>
+          </div>
+        </div>
+        <div>
+          <button className="btn btn-outline-danger">CANCELAR</button>
+          <button className="btn btn-primary">SALVAR</button>
+        </div>
+      </form>
+    </div>
+  </div>
+);
+
+export default Form;
