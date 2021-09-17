4851ed9088a260b626ba3609f2e8f5d048e436eb
Tiago Baratieri <tiagobaratieri97@gmail.com>
2021-09-17 08:24:05 -0300

    product listing from backend

--- front-web/src/pages/Admin/Products/List/index.tsx
+++ front-web/src/pages/Admin/Products/List/index.tsx
@@ -1,32 +1,30 @@
+import { AxiosRequestConfig } from 'axios';
 import ProductCrudCard from 'components/ProductCrudCard/indexs';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Product } from 'type/product';
+import { SpringPage } from 'type/vendor/spring';
+import { requestBackend } from 'util/request';
 import './styles.scss';
 
 const List = () => {
-  const product = {
-    id: 1,
-    name: 'The Lord of the Rings',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    price: 90.5,
-    imgUrl:
-      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg',
-    date: '2020-07-13T20:50:07.123450Z',
-    categories: [
-      {
-        id: 2,
-        name: 'Eletrônicos',
+  const [page, setPage] = useState<SpringPage<Product>>();
+
+  useEffect(() => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/products',
+      params: {
+        page: 0,
+        size: 50,
       },
-      {
-        id: 1,
-        name: 'Computadores',
-      },
-      {
-        id: 1,
-        name: 'Celulares',
-      },
-    ],
-  };
+    };
+
+    requestBackend(config).then((response) => {
+      setPage(response.data);
+    });
+  }, []);
+
   return (
     <div className="product-crud-container">
       <div className="product-crud-bar-container">
@@ -39,15 +37,11 @@ const List = () => {
         <div className="base-card product-filter-container">Search Bar</div>
       </div>
       <div className="row">
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        <div className="col-sm-6  col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        <div className="col-sm-6  col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
+        {page?.content.map((product) => (
+          <div key={product.id} className="col-sm-6 col-md-12">
+            <ProductCrudCard product={product} />
+          </div>
+        ))}
       </div>
     </div>
   );
