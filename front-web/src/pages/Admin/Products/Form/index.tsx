f072bd290e5e89d7bbe38d29d0e6acf91e9bb1e8
Tiago Baratieri <tiagobaratieri97@gmail.com>
2021-09-17 09:50:40 -0300

    Product save

--- front-web/src/pages/Admin/Products/Form/index.tsx
+++ front-web/src/pages/Admin/Products/Form/index.tsx
@@ -1,43 +1,116 @@
+import { AxiosRequestConfig } from 'axios';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router';
+import { Product } from 'type/product';
+import { requestBackend } from 'util/request';
 import './styles.scss';
 
-const Form = () => (
-  <div className="product-crud-container">
-    <div className="base-card product-crud-form-card">
-      <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
-      <form action="">
-        <div className="row prodduct-crud-inputs-container">
-          <div className="col-lg-6 prodduct-crud-input-left-container">
-            <div className="margin-bottom-30">
-              <input type="text" className="form-control base-input" />
+const Form = () => {
+  const history = useHistory();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Product>();
+
+  const onSubmit = (formData: Product) => {
+    const data = {
+      ...formData,
+      imgUrl:
+        'https://media.gettyimages.com/photos/living-room-with-a-microsoft-xbox-series-x-home-video-game-console-a-picture-id1229473400?k=20&m=1229473400&s=612x612&w=0&h=ZHcK1UEMY0btQXP2brvKpVHJhnD8NVfUwaCwd2FTjAs=',
+      categories: [{ id: 1, name: '' }],
+    };
+    const config: AxiosRequestConfig = {
+      method: 'POST',
+      url: '/products',
+      data,
+      withCredentials: true,
+    };
+
+    requestBackend(config).then((response) => {
+      console.log(response.data);
+    });
+  };
+
+  const handleCancel = () => {
+    history.push('/admin/products');
+  };
+
+  return (
+    <div className="product-crud-container">
+      <div className="base-card product-crud-form-card">
+        <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="row prodduct-crud-inputs-container">
+            <div className="col-lg-6 prodduct-crud-input-left-container">
+              <div className="margin-bottom-30">
+                <input
+                  {...register('name', {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.name ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Nome do produto"
+                  name="name"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.name?.message}
+                </div>
+              </div>
+              <div className="margin-bottom-30">
+                <input
+                  {...register('price', {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.name ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Preço"
+                  name="price"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.price?.message}
+                </div>
+              </div>
             </div>
-            <div className="margin-bottom-30">
-              <input type="text" className="form-control base-input" />
-            </div>
-            <div>
-              <input type="text" className="form-control base-input" />
+            <div className="col-lg-6">
+              <div>
+                <textarea
+                  rows={10}
+                  {...register('description', {
+                    required: 'Campo obrigatório',
+                  })}
+                  className={`form-control base-input ${
+                    errors.name ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Descrição"
+                  name="description"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.description?.message}
+                </div>
+              </div>
             </div>
           </div>
-          <div className="col-lg-6">
-            <div>
-              <textarea
-                name=""
-                rows={10}
-                className="form-control base-input h-auto"
-              />
-            </div>
+          <div className="product-crud-buttons-container">
+            <button
+              className="btn btn-outline-danger product-crud-button"
+              onClick={handleCancel}
+            >
+              CANCELAR
+            </button>
+            <button className="btn btn-primary product-crud-button text-white">
+              SALVAR
+            </button>
           </div>
-        </div>
-        <div className="product-crud-buttons-container">
-          <button className="btn btn-outline-danger product-crud-button">
-            CANCELAR
-          </button>
-          <button className="btn btn-primary product-crud-button text-white">
-            SALVAR
-          </button>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Form;
