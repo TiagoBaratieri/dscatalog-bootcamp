afd47215ce857e8ae9e6dcb2b61ad16fc383e847
Tiago Baratieri <tiagobaratieri97@gmail.com>
2021-09-18 08:13:35 -0300

    Refactor + useCallback

--- front-web/src/pages/Admin/Products/List/index.tsx
+++ front-web/src/pages/Admin/Products/List/index.tsx
@@ -1,34 +1,46 @@
 import { AxiosRequestConfig } from 'axios';
 import Pagination from 'components/Pagination';
 import ProductCrudCard from 'components/ProductCrudCard/indexs';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Product } from 'type/product';
 import { SpringPage } from 'type/vendor/spring';
 import { requestBackend } from 'util/request';
 import './styles.scss';
 
+type ControlComponentsData = {
+  activePage: number;
+};
+
 const List = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
 
-  useEffect(() => {
-    getProducts(0);
-  }, []);
+  const [controlComponentsData, setControlComponentsData] =
+    useState<ControlComponentsData>({
+      activePage: 0,
+    });
 
-  const getProducts = (pageNumber: number) => {
+  const handlePageChange = (pageNumber: number) => {
+    setControlComponentsData({ activePage: pageNumber });
+  };
+
+  const getProducts = useCallback(() => {
     const config: AxiosRequestConfig = {
       method: 'GET',
       url: '/products',
       params: {
-        page: pageNumber,
+        page: controlComponentsData.activePage,
         size: 3,
       },
     };
-
     requestBackend(config).then((response) => {
       setPage(response.data);
     });
-  };
+  }, [controlComponentsData]);
+
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
 
   return (
     <div className="product-crud-container">
@@ -52,6 +64,7 @@ const List = () => {
         forcePage={page?.number}
         pageCount={page ? page.totalPages : 0}
         range={3}
+        onChange={handlePageChange}
       />
     </div>
   );
