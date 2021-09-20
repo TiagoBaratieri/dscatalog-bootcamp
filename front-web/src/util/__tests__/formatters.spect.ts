08c5d807c9e19dcd4d643c771b476598595345f5
TiagoBaratieri <tiagobaratieri97@gmail.com>
2021-09-20 15:28:20 -0300

    First test

---
+++ front-web/src/util/__tests__/formatters.spect.ts
@@ -0,0 +1,9 @@
+import { formatPrice } from "util/formatters";
+
+test('formatPrice should format number pt-BR when given 10.1', () => {
+    const value = 10.1;
+
+    const result = formatPrice(value);
+
+    expect(result).toEqual("10.10");
+})
