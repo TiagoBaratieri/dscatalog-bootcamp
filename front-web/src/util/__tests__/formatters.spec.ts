87f852c8e396f386e9499b4982ddc4604a10786a
TiagoBaratieri <tiagobaratieri97@gmail.com>
2021-09-21 12:33:17 -0300

    Mocking with spyOn

--- front-web/src/util/__tests__/formatters.spec.ts
+++
@@ -1,6 +0,0 @@
-import { formatPrice } from "util/formatters";
-
-it('should format number pt-BR when given 10.1', () => {
-    const result = formatPrice(10.1);
-    expect(result).toEqual('10,10');
-});
