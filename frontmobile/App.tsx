6c24b62b8168dce7f6bf102adbd89eec658b4c37
Tiago Baratieri <tiagobaratieri97@gmail.com>
2021-09-27 16:16:36 -0300

    frontmobile created

---
+++ frontmobile/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {View, Text } from "react-native";
+
+const App = () => {
+  return (
+    <View
+    style={{
+      backgroundColor: "#069",
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+    }}
+    >
+      <Text>Olá, mundo</Text>
+    </View>
+  )
+};
+ 
+   
+
+export default App;
